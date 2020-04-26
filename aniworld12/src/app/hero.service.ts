a1fab82e33f72c6216c4879ac7c6d1d18fd586f3
BigBabyDake <47224883+danialzinolla@users.noreply.github.com>
2020-04-26 21:06:55 +0300

    Add files via upload

---
+++ aniworld12/src/app/hero.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Hero } from './hero';
+
+
+@Injectable({ providedIn: 'root' })
+export class HeroService {
+
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(
+    private http: HttpClient) { }
+
+  /** GET heroes from the server */
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        );
+  }
+
+  /** GET hero by id. Return `undefined` when id not found */
+  getHeroNo404<Data>(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/?id=${id}`;
+    return this.http.get<Hero[]>(url)
+      .pipe(
+        map(heroes => heroes[0]), // returns a {0|1} element array
+       );
+  }
+
+  /** GET hero by id. Will 404 if id not found */
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe();
+  }
+
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe();
+  }
+
+}
---
+++ aniworld12/src/app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 1, name: "Shaman King" },
+  { id: 2, name: "JoJo" },
+  { id: 3, name: "Bleach" },
+  { id: 4, name: "Demon slayer" },
+  { id: 5, name: "Danmachi" },
+  { id: 6, name: "My hero academia" },
+  { id: 7, name: "Haikyuu" },
+  { id: 8, name: "No life no game" },
+  { id: 9, name: "Kuroko no Basuke" },
+  { id: 10, name: "Tower of God" },
+  { id: 11, name: "Dr Stone" },
+  { id: 12, name: "Naruto" },
+  { id: 13, name: "One Piece" },
+  { id: 14, name: "Mob Psycho" },
+  { id: 15, name: "High School DxD" },
+  { id: 16, name: "Plunderer" },
+  { id: 17, name: "Shoumetsu Toshi" },
+  { id: 18, name: "One punch man" },
+  { id: 19, name: "Toradora" },
+  { id: 20, name: "SAO" },
+];
